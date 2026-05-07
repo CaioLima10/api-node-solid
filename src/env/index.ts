4cf377fd4367351e870f0d18aed162158f594a49
CaioLima <caiohenrys50@gmail.com>
2026-05-06 22:11:42 -0300

    aliases

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,19 @@
-import "dotenv/config";
-import { z } from "zod";
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
 })
 
-const _env = envSchema.safeParse(process.env);
+const _env = envSchema.safeParse(process.env)
 
-if(!_env.success) {
-  console.error('❌ Invalid environment variables', _env.error.format());
+if (!_env.success) {
+  console.error('❌ Invalid environment variables', _env.error.format())
 
   throw new Error('❌Invalid environment variables')
 }
 
-const env = _env.data;
+const env = _env.data
 
 export { env }
